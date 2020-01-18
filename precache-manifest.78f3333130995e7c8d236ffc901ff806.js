8e0c0d0760bac140c95fd3ba9f8f479d3f4a3d0c
natgonzalezrosa <natgonzalezrosa@gmail.com>
2020-01-18 16:37:07 -0500

    Updates

---
+++ precache-manifest.78f3333130995e7c8d236ffc901ff806.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f8a5459c011465f0325514d7ab16a5",
+    "url": "/react-redux-tutorial/index.html"
+  },
+  {
+    "revision": "e3daddd0e88b468e044f",
+    "url": "/react-redux-tutorial/static/js/2.3e36b3fd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-redux-tutorial/static/js/2.3e36b3fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7458d5ada0d6a7e1a587",
+    "url": "/react-redux-tutorial/static/js/main.73c7d6c1.chunk.js"
+  },
+  {
+    "revision": "3f95e443b8b5639d19a9",
+    "url": "/react-redux-tutorial/static/js/runtime-main.81be91b9.js"
+  }
+]);
