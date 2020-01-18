4fb2ff1e7c30ef1f01c784746b0806c6e636d716
natgonzalezrosa <natgonzalezrosa@gmail.com>
2020-01-18 13:26:58 -0500

    update Post component and add redux-thunk

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,5 +1,18 @@
 import { ADD_ARTICLE } from "../constants/action-types";
+import { DATA_LOADED } from "../constants/action-types";
 
+// ACTION CREATOR
 export function addArticle(payload) {
     return { type: ADD_ARTICLE, payload };
 };
+
+// our new action creator
+export function getData() {
+    return function(dispatch) {
+        return fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(json => {
+            dispatch({ type: DATA_LOADED, payload: json });
+        });
+    };
+}
